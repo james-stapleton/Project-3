0614359da5587a7d2a87ac55f07660ad3a4c14ce
“jms” <jstapleton536@gmail.com>
2022-10-26 10:44:06 -0400

    recipe page

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import SavedDrinks from "./pages/SavedDrinks";
 import UploadDrink from "./pages/UploadDrink";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
+import Recipe from './pages/Recipe'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
@@ -16,6 +17,13 @@ const client = new ApolloClient({
 });
 
 function App() {
+
+
+  function loadDrink(e) {
+    console.log(e.target.id)
+    
+}
+
   return (
     <ApolloProvider client={client}>
       <Router>
@@ -25,13 +33,15 @@ function App() {
           
           <Route path="/MostViewedDrinks" element={<MostViewedDrinks />} />
 
-          <Route path="/CocktailRankings" element={<CocktailRankings />} />
+          <Route path="/CocktailRankings" element={<CocktailRankings loadDrink = {loadDrink} />} />
 
           <Route path="/SavedDrinks" element={<SavedDrinks />} />
 
           <Route path="/UploadDrink" element={<UploadDrink />} />
 
           <Route path="/Login" element={<Login />} />
+
+          <Route path = 'Recipe/:name' element = {<Recipe />} /> 
 
           <Route path="*" element={<NotFound />} />
         </Routes>
