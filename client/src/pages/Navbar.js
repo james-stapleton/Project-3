fa8c55ee01c9804047f4836337830f93c7bc7314
Christopher C Ray <110488249+cray412@users.noreply.github.com>
2022-10-27 14:17:00 -0400

    intial header setup

--- client/src/pages/Navbar.js
+++ client/src/pages/Navbar.js
@@ -9,7 +9,6 @@ const Navbar = () => {
       <Link to = "/MostViewedDrinks"><button className="btn btn-outline-success" type="button">Most Viewed Cocktails</button></Link>
       <Link to ="/CocktailRankings"><button className="btn btn-outline-success" type="button">Cocktail Rankings</button></Link>
       <Link to = "/SavedDrinks"><button className="btn btn-outline-success" type="button">Saved Drinks</button></Link>
-      <Link to="/"><h1 className="text-white">Cocktail Curator</h1></Link>
       <Link to="/UploadDrink"><button className="btn btn-outline-success" type="button">Upload Drink</button></Link>
       <Link to="Login"><button className="btn btn-outline-success" type="button">Login</button></Link>
       <Link to="Login"><button className="btn btn-outline-success" type="button">Logout</button></Link>
