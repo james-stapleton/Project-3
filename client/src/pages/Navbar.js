9658170e15893215aab96db3c6706cd77e560f1a
Christopher C Ray <110488249+cray412@users.noreply.github.com>
2022-10-28 13:37:13 -0400

    Finished page and login button formatting

--- client/src/pages/Navbar.js
+++ client/src/pages/Navbar.js
@@ -10,21 +10,21 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <form className="container form-inline">
-        <div class="row bg-banner" width="100%">
-          <div class="col image-container">
+      <form className="container-fluid form-inline">
+        <div class="row bg-banner">
+          <div class="col-4 image-container">
             <a href="/"><img src="../images/Logo_Finished.png" width="398" alt="Logo" class="logo" /></a>
+          </div>
+          <div class="col-6 page-buttons">
+            <Link reloadDocument to="/MostViewedDrinks" ><button class="button" type="button">Most Viewed Cocktails</button></Link>
+            <Link reloadDocument to="/CocktailRankings"><button className="button" type="button">Cocktail Rankings</button></Link>
+            <Link reloadDocument to="/SavedDrinks"><button className="button" type="button">Saved Drinks</button></Link>
+            <Link to="/UploadDrink"><button className="button" type="button">Upload Drink</button></Link>
           </div>
           <div class="col login-buttons">
             <Link to="Login"><button type="button">Login</button></Link>
             <Link to="Login"><button type="button">Logout</button></Link>
             <Link to="/Register"><button>Register</button></Link>
-          </div>
-          <div class="col">
-            <Link reloadDocument to="/MostViewedDrinks" ><button className="btn btn-outline-success" type="button">Most Viewed Cocktails</button></Link>
-            <Link reloadDocument to="/CocktailRankings"><button className="btn btn-outline-success" type="button">Cocktail Rankings</button></Link>
-            <Link reloadDocument to="/SavedDrinks"><button className="btn btn-outline-success" type="button">Saved Drinks</button></Link>
-            <Link to="/UploadDrink"><button className="btn btn-outline-success" type="button">Upload Drink</button></Link>
           </div>
         </div>
       </form>
