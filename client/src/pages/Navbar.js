537c6c1f34a6b557ec4c39b6a120fabbbf32a3b0
Christopher C Ray <110488249+cray412@users.noreply.github.com>
2022-10-28 11:52:07 -0400

    Setup Bootstrap Layout for Header and Navbar

--- client/src/pages/Navbar.js
+++ client/src/pages/Navbar.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import "./Navbar.css";
 
 
 const Navbar = () => {
 
-    const refreshPage = () => window.location.reload(); 
-    
+  const refreshPage = () => window.location.reload();
 
-    return (
+
+  return (
     <nav className="navbar">
-    <form className="form-inline">
-      <Link reloadDocument to = "/MostViewedDrinks" ><button className="btn btn-outline-success" type="button">Most Viewed Cocktails</button></Link>
-      <Link reloadDocument to ="/CocktailRankings"><button className="btn btn-outline-success" type="button">Cocktail Rankings</button></Link>
-      <Link reloadDocument to = "/SavedDrinks"><button className="btn btn-outline-success" type="button">Saved Drinks</button></Link>
-      <Link to="/"><h1 className="text-white">Cocktail Curator</h1></Link>
-      <Link to="/UploadDrink"><button className="btn btn-outline-success" type="button">Upload Drink</button></Link>
-      <Link to="Login"><button className="btn btn-outline-success" type="button">Login</button></Link>
-      <Link to="Login"><button className="btn btn-outline-success" type="button">Logout</button></Link>
-      <Link to ="/Register"><button>Register</button></Link>
-    </form>
-  </nav> 
+      <form className="container form-inline">
+        <div class="row bg-banner" width="100%">
+          <div class="col image-container">
+            <a href="/"><img src="../images/Logo_Finished.png" width="398" alt="Logo" class="logo" /></a>
+          </div>
+          <div class="col login-buttons">
+            <Link to="Login"><button type="button">Login</button></Link>
+            <Link to="Login"><button type="button">Logout</button></Link>
+            <Link to="/Register"><button>Register</button></Link>
+          </div>
+          <div class="col">
+            <Link reloadDocument to="/MostViewedDrinks" ><button className="btn btn-outline-success" type="button">Most Viewed Cocktails</button></Link>
+            <Link reloadDocument to="/CocktailRankings"><button className="btn btn-outline-success" type="button">Cocktail Rankings</button></Link>
+            <Link reloadDocument to="/SavedDrinks"><button className="btn btn-outline-success" type="button">Saved Drinks</button></Link>
+            <Link to="/UploadDrink"><button className="btn btn-outline-success" type="button">Upload Drink</button></Link>
+          </div>
+        </div>
+      </form>
+    </nav>
   )
 }
 
